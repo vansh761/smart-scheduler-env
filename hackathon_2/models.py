7d55e4996d465e8588c6a6096b223139d6d9a320
Vansh Gupta <vanshgupta151006@gmail.com>
2026-04-09 21:07:01 +0530

    Update models.py

--- hackathon_2/models.py
+++ hackathon_2/models.py
@@ -1,6 +1,6 @@
 from openenv.core.env_server.types import Action, Observation
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import List, Optional, Literal
 
 # -------- TASK MODEL -------- #
 class Task(BaseModel):
